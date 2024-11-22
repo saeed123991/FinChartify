a4d7e0b9c5efe4ed3583843d9b32ca11496cb3ca
saeed123991 <79787125+saeed123991@users.noreply.github.com>
2024-11-23 01:58:31 +0500

    added config.py file which keeps the db and secret key

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = '3FnMfQvjlsgjd!k1#PXhck89LP_8A5#TG4EylTzNE@1tZcCZ'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///expenses.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
