e558121385b0704ad1b4521d19320e7ed918d5a0
saeed123991 <79787125+saeed123991@users.noreply.github.com>
2024-11-24 20:11:56 +0000

    implemented the default route in app.py

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ app = Flask(__name__, template_folder='UI', static_folder='assets')
 app.config.from_object(Config)
 db.init_app(app)
 
-
+# this is the default route or landing page of finChartify
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # from here my app finChartify will start
 if __name__ == '__main__':
