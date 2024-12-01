2ba27114c399e2f19b7fe9fc4ae3a0edaa537e0c
saeed123991 <79787125+saeed123991@users.noreply.github.com>
2024-12-01 18:32:45 +0000

    make some variables and added fetch expense function in script.js file

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
  
+    const addExpenseModal = document.getElementById('expense-modal');
+    const expenseForm = document.getElementById('expense-form');
+    const expenseDate = document.getElementById('expense-date');
+    const expenseName = document.getElementById('expense-name');
+    const expenseDescription = document.getElementById('expense-description');
+    const expenseAmount = document.getElementById('expense-amount');
+    const modalTitle = document.getElementById('modal-title');
+    const submitBtn = document.getElementById('submit-btn');
+    const closeBtn = document.querySelector('.close-btn');
+    const chartBtn = document.createElement('button');
+    const exportExcelBtn = document.querySelector('.export-logout .btn');
+    const expenseTable = document.getElementById('expense-table').getElementsByTagName('tbody')[0];
+    const searchInput = document.getElementById('search-input');
+    const chartModal = document.getElementById('chart-modal');
+    const chartCloseBtn = chartModal.querySelector('.chart-close-btn');
+    const expenseChart = document.getElementById('expense-chart').getContext('2d');
+    let chartInstance;
+    let editingExpenseId = null;
 
+    chartBtn.textContent = "Show Chart";
+    chartBtn.className = "btn";
+    chartBtn.style.display = 'none';
+    searchInput.parentElement.appendChild(chartBtn);
+
+    const fetchExpenses = async () => {
+        const response = await fetch('/expenses', { method: 'GET' });
+        const expenses = await response.json();
+        return expenses;
+    };
 
 
     (async () => {
