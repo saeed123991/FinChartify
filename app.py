74641c68a80aa099e82d0fd0dc206ed22ae0ea1b
saeed123991 <79787125+saeed123991@users.noreply.github.com>
2024-12-01 18:26:08 +0000

    imported datatime function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify, make_response
+from datetime import datetime
 from models import db, User, Expense
 from config import Config
 
